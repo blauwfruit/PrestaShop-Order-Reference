2126f1ebe0203bc40bd24c03eceaba3c5e26913f
Matthijs <github@muyncky.eu>
2021-07-09 14:54:54 +0200

    style: add license to upgrade file 1.2.0

--- upgrade/upgrade-1.2.0.php
+++ upgrade/upgrade-1.2.0.php
@@ -1,4 +1,14 @@
 <?php
+/**
+*   Orderreference
+*
+*   Do not copy, modify or distribute this document in any form.
+*
+*   @author     @gett-thijssimonis
+*   @copyright  Copyright (c) 2013-2021 blauwfruit (http://blauwfruit.nl)
+*   @license    Proprietary Software
+*
+*/
 
 function upgrade_module_1_2_0(Module $module)
 {
